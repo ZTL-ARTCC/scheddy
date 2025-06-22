5b912d0c34a4ab88ad38344fe3b7c7abbb87645a
Ian Cowan <ian@cowan.aero>
2025-06-22 17:20:11 -0600

    Format

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'drizzle-kit';
-if (!process.env.SCHEDDY_DATABASE_URL && !process.env.DATABASE_URL) throw new Error('SCHEDDY_DATABASE_URL is not set');
+if (!process.env.SCHEDDY_DATABASE_URL && !process.env.DATABASE_URL)
+	throw new Error('SCHEDDY_DATABASE_URL is not set');
 
 export default defineConfig({
 	schema: './src/lib/server/db/schema.ts',
 
 	dbCredentials: {
-		url: process.env.SCHEDDY_DATABASE_URL || process.env.DATABASE_URL,
+		url: process.env.SCHEDDY_DATABASE_URL || process.env.DATABASE_URL
 	},
 
 	verbose: true,
