ca12116103964b498a88df39d07c416abfbdc618
core <core@coredoes.dev>
2025-11-02 16:24:49 -0500

    chore: extract auth role function into $lib/helpers

--- src/routes/callback/+page.server.ts
+++ src/routes/callback/+page.server.ts
@@ -5,6 +5,7 @@ import { nanoid } from 'nanoid';
 import { redirect } from '@sveltejs/kit';
 import { ROLE_DEVELOPER, ROLE_STAFF, ROLE_MENTOR, ROLE_STUDENT } from '$lib/utils';
 import { serverConfig } from '$lib/config/server';
+import { determineHighestRole } from '$lib/helpers/auth';
 
 export const load: PageServerLoad = async ({ cookies, url, fetch }) => {
 	if (url.searchParams.has('error')) {
@@ -107,42 +108,7 @@ export const load: PageServerLoad = async ({ cookies, url, fetch }) => {
 
 	const vatusa_info = await vatusa_user_resp.json();
 
-	let highest_role = 0;
-
-	// If they're a home controller, give them student perms
-	if (vatusa_info.data.facility == serverConfig.facility.id) {
-		if (ROLE_STUDENT > highest_role) {
-			highest_role = ROLE_STUDENT;
-		}
-	}
-
-	// If they're a visiting controller, give them student perms
-	for (const visiting_facility of vatusa_info.data.visiting_facilities) {
-		if (visiting_facility.facility == serverConfig.facility.id && ROLE_STUDENT > highest_role) {
-			highest_role = ROLE_STUDENT;
-		}
-	}
-
-	// Assign staff roles
-	// WM: 							ROLE_DEVELOPER
-	// ATM, DATM, TA: 	ROLE_STAFF
-	// INS, MTR: 				ROLE_MENTOR
-	for (const role of vatusa_info.data.roles) {
-		if (role.facility == serverConfig.facility.id) {
-			let this_role = 0;
-			if (role.role == 'WM') {
-				this_role = ROLE_DEVELOPER;
-			} else if (role.role == 'ATM' || role.role == 'DATM' || role.role == 'TA') {
-				this_role = ROLE_STAFF;
-			} else if (role.role == 'INS' || role.role == 'MTR') {
-				this_role = ROLE_MENTOR;
-			}
-
-			if (this_role > highest_role) {
-				highest_role = this_role;
-			}
-		}
-	}
+	const highest_role = determineHighestRole(vatusa_info);
 
 	// User must be at least a ROLE_STUDENT to log in
 	if (highest_role < ROLE_STUDENT) {
